1ba6e0e1d82eb34dc3f730bced307dcea5edded9
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-10 19:57:36 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110194452) do
+ActiveRecord::Schema.define(version: 20180110195651) do
 
   create_table "tweets", force: :cascade do |t|
     t.string  "content"
     t.integer "user_id"
   end
 
-  create_table "user", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "password_digest"
